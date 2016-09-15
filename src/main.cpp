6a2958df8dc46db3de30ee65f91193e6561054e5
João Faria <joao.faria@astro.up.pt>
2016-09-15 16:47:48 +0100

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,8 @@ using namespace DNest4;
 int main(int argc, char** argv)
 {
 
-	//DataSet& full = DataSet::getRef("full");
-	//full.load("data/k10_harpsn.dat", "kms", 0);
-
-	//DataSet& harps = DataSet::getRef("HARPS");
-	//DataSet& sophie = DataSet::getRef("SOPHIE");
-	//harps.load("test_offsets1.rdb", "ms", 1);
-	//sophie.load("test_offsets2.rdb", "ms", 2);
-
-	//cout<<DataSet().nsites()<<endl;
-	//cout<<DataSet::getRef("full").N;
-	//cout<<endl;
-
-	//Data::get_instance().load("fake_data.txt");
 	Data::get_instance().loadnew("data/PlSy14.rdb");
-
     //return 0;
-
-
 
 	start<MyModel>(argc, argv);
 	return 0;
