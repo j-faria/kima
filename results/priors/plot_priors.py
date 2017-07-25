08b8ad47f413ddcbf2bbc55e9daa3ba40c582613
João Faria <joao.faria@astro.up.pt>
2017-07-25 17:06:18 +0200

    right prior plots now

--- results/priors/plot_priors.py
+++ results/priors/plot_priors.py
@@ -2,7 +2,9 @@ from pylab import *
 import subprocess
 
 parameters = ['s', 'alpha', 'Lambda_e', 'Lambda_p', 'tau',
-              'P', 'K', 'ecc', 'w', 'phi',
+              None, None, None,
+              'P', 'K', 'w', 'ecc', 'phi',
+              None,
               'C']
 
 import sys
@@ -11,5 +13,6 @@ from check_priors import do_plot, get_column
 
 
 for i, par in enumerate(parameters):
+    if par is None: continue
     data = get_column(i+1)
     do_plot(data, par, save='prior_%s.png' % par)
--- scripts/check_priors.py
+++ scripts/check_priors.py
@@ -5,38 +5,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astroML.plotting import hist
 
-column = int(sys.argv[1])
-
-try:
-	log = sys.argv[2] == 'log'
-except IndexError:
-	log = False
-
-with open('sample.txt') as f:
-# with open('posterior_sample.txt') as f:
-	firstline = f.readline()
-firstline = firstline.strip().replace('#','')
-names = firstline.split()
-
-try:
-	print 'Histogram of column %d: %s' % (column, names[column-1])
-except IndexError:
-	print 'Histogram of column %d' % column
-
-data = np.loadtxt('sample.txt', unpack=True, usecols=(column-1,))
+def do_plot(data, name, column=1, save=None):
+    plt.figure()
+    bins = 100 #np.linspace(data.min(), data.max(), 100)
+    plt.hist(data, bins=bins, color='black', histtype='step', normed=True)
+    # if log: plt.xscale('log')
+    # hist(data, bins='knuth', color='black', histtype='step', normed=True)
+    if save:
+        plt.savefig(save)
+    else:
+        plt.show()
 
 
-plt.figure()
+def get_column(column):
+    return np.loadtxt('sample.txt', unpack=True, usecols=(column-1,))
 
-if log:
-	data = data[np.nonzero(data)[0]]
-	data = np.log(data)
 
-# if log:
-	# bins = np.logspace(np.log(data.min()), np.log(data.max()), 100)
-# else:
-bins = 100 #np.linspace(data.min(), data.max(), 100)
-plt.hist(data, bins=bins, color='black', histtype='step', normed=True)
-# if log: plt.xscale('log')
-# hist(data, bins='knuth', color='black', histtype='step', normed=True)
-plt.show()
+if __name__ == '__main__':
+    column = int(sys.argv[1])
+    try:
+        log = sys.argv[2] == 'log'
+    except IndexError:
+        log = False
+
+    with open('sample.txt') as f:
+    # with open('posterior_sample.txt') as f:
+        firstline = f.readline()
+    firstline = firstline.strip().replace('#','')
+    names = firstline.split()
+
+    try:
+        print 'Histogram of column %d: %s' % (column, names[column-1])
+    except IndexError:
+        print 'Histogram of column %d' % column
+
+    data = get_column(column)
+    # data = np.loadtxt('sample.txt', unpack=True, usecols=(column-1,))
+
+    if log:
+        data = data[np.nonzero(data)[0]]
+        data = np.log(data)
+
+    # if log:
+        # bins = np.logspace(np.log(data.min()), np.log(data.max()), 100)
+    # else:
+
+    do_plot(data, column)
