b616ef30762e2069e8569b6c771eae246cfd7675
João Faria <joao.faria@astro.up.pt>
2018-10-03 21:19:09 +0100

    multi bool also in the template

--- pykima/template/kima_setup.cpp
+++ pykima/template/kima_setup.cpp
@@ -10,6 +10,7 @@ const bool obs_after_HARPS_fibers = false;
 const bool GP = false;
 const bool hyperpriors = false;
 const bool trend = false;
+const bool multi_instrument = true;
 
 RVmodel::RVmodel():fix(true),npmax(1)
 {
