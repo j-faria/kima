7f39e1d5896a8b6c658c2555bdd8cb34be42bb3e
jdavidrcamacho <jdavidrcamacho@gmail.com>
2018-10-24 18:20:55 +0100

    still didn't understood what a getter is but it works

--- src/Data.cpp
+++ src/Data.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <boost/lambda/lambda.hpp>
 
 using namespace std;
 
@@ -21,127 +22,125 @@ Data::Data(){}
 //-----------------------------------------------------------------------------
 // Let's overload the stream input operator to read a list of CSV fields (which a CSV record).
 istream& operator >> ( istream& ins, record_t& record )
-  {
-  // make sure that the returned record contains only the stuff we read now
-  record.clear();
+{
+    // make sure that the returned record contains only the stuff we read now
+    record.clear();
 
-  // read the entire line into a string (a CSV record is terminated by a newline)
-  string line;
-  getline( ins, line );
+    // read the entire line into a string (a CSV record is terminated by a newline)
+    string line;
+    getline( ins, line );
 
-  // now we'll use a stringstream to separate the fields out of the line
-  stringstream ss( line );
+    // now we'll use a stringstream to separate the fields out of the line
+    stringstream ss( line );
 
-  // convert each field to a double and
-  // add the newly-converted field to the end of the record
-  double f;
-  while (ss >> f)
-    record.push_back(f);
+    // convert each field to a double and
+    // add the newly-converted field to the end of the record
+    double f;
+    while (ss >> f)
+        record.push_back(f);
 
-  // Now we have read a single line, converted into a list of fields, converted the fields
-  // from strings to doubles, and stored the results in the argument record, so
-  // we just return the argument stream as required for this kind of input overload function.
-  return ins;
-  }
+    // Now we have read a single line, converted into a list of fields, converted the fields
+    // from strings to doubles, and stored the results in the argument record, so
+    // we just return the argument stream as required for this kind of input overload function.
+    return ins;
+}
 
 //-----------------------------------------------------------------------------
 // Let's likewise overload the stream input operator to read a list of CSV records.
 // This time it is a little easier, just because we only need to worry about reading
 // records, and not fields.
 istream& operator >> ( istream& ins, data_t& data )
-  {
-  // used to hold the header
-  data_t header;
-  // make sure that the returned data only contains the CSV data we read here
-  data.clear();
+{
+    // used to hold the header
+    data_t header;
+    // make sure that the returned data only contains the CSV data we read here
+    data.clear();
 
-  // For every record we can read from the file, append it to our resulting data
-  // except if it's in the header
-  int i = 0;
-  record_t record;
-  while (ins >> record)
+    // For every record we can read from the file, append it to our resulting data
+    // except if it's in the header
+    int i = 0;
+    record_t record;
+    while (ins >> record)
     {
-      //if (i==0 || i==1) header.push_back(record);
-      data.push_back(record);
-      //  i++;
+        //if (i==0 || i==1) header.push_back(record);
+        data.push_back(record);
+        //  i++;
     }
 
-  // Again, return the argument stream
-  return ins;
-  }
+    // Again, return the argument stream
+    return ins;
+}
 
 
 void Data::load(const char* filename, const char* units, int skip)
-  /*
-  Read in tab/space separated file `filename` with columns
-  jdb	vrad	svrad	fwhm	bis_span	rhk	sig_rhk
-  ---	----	-----	----	--------	---	-------
-  where vrad and error are in `units` (either "kms" or "ms")
-  */
-  {
+    /*
+    Read in tab/space separated file `filename` with columns
+    jdb   vrad    svrad   fwhm    bis_span    rhk sig_rhk
+    ---   ----    -----   ----    --------    --- -------
+    where vrad and error are in `units` (either "kms" or "ms")
+    */
+{
 
-  data_t data;
+    data_t data;
 
-  // Empty the vectors
-  t.clear();
-  rv.clear();
-  rverr.clear();
-  fwhm.clear();
-  bis.clear();
-  rhk.clear();
-  rhkerr.clear();
+    // Empty the vectors
+    t.clear();
+    rv.clear();
+    rverr.clear();
+    fwhm.clear();
+    bis.clear();
+    rhk.clear();
+    rhkerr.clear();
 
-  // Read the file into the data container
-  ifstream infile( filename );
-  infile >> data;
-  //operator>>(infile, data, skip);
+    // Read the file into the data container
+    ifstream infile( filename );
+    infile >> data;
+    //operator>>(infile, data, skip);
 
 
-  // Complain if something went wrong.
-  if (!infile.eof())
-  {
-    printf("Could not read data file (%s)!\n", filename);
-    exit(1);
-  }
-
-  infile.close();
-
-  datafile = filename;
-  dataunits = units;
-  dataskip = skip;
-  
-  double factor = 1.;
-  if(units == "kms") factor = 1E3;
-
-  for (unsigned n = 0; n < data.size(); n++)
+    // Complain if something went wrong.
+    if (!infile.eof())
     {
-      if (n<skip) continue;
-      t.push_back(data[n][0]);
-      rv.push_back(data[n][1] * factor);
-      rverr.push_back(data[n][2] * factor);
-      fwhm.push_back(data[n][3]);
-      bis.push_back(data[n][4] * factor);
-      rhk.push_back(data[n][5]);
-      rhkerr.push_back(data[n][6]);
+        printf("Could not read data file (%s)!\n", filename);
+        exit(1);
     }
 
-  // How many points did we read?
-  printf("# Loaded %d data points from file %s\n", t.size(), filename);
-  if(units == "kms") printf("# Multiplied all RVs by 1000; units are now m/s.\n");
-  
-  for(unsigned i=0; i<data.size(); i++)
-  {
-      if (t[i] > 57170.)
-      {
-          index_fibers = i;
-          break;
-      }
-  }
-//  get_y();
-//  get_fwhmerr();
-//  get_biserr();
-  get_sig();
-  }
+    infile.close();
+
+    datafile = filename;
+    dataunits = units;
+    dataskip = skip;
+
+    double factor = 1.;
+    if(units == "kms") factor = 1E3;
+
+    for (unsigned n = 0; n < data.size(); n++)
+    {
+        if (n<skip) continue;
+        t.push_back(data[n][0]);
+        rv.push_back(data[n][1] * factor);
+        rverr.push_back(data[n][2] * factor);
+        fwhm.push_back(data[n][3]);
+        bis.push_back(data[n][4] * factor);
+        rhk.push_back(data[n][5]);
+        rhkerr.push_back(data[n][6]);
+    }
+
+    // How many points did we read?
+    printf("# Loaded %d data points from file %s\n", t.size(), filename);
+    if(units == "kms") printf("# Multiplied all RVs by 1000; units are now m/s.\n");
+
+    for(unsigned i=0; i<data.size(); i++)
+    {
+        if (t[i] > 57170.)
+        {
+        index_fibers = i;
+        break;
+        }
+    }
+    get_y();
+    get_sig();
+}
 
 
 
@@ -158,52 +157,59 @@ double Data::get_rv_var() const
 }
 
 
-// functor for getting sum of previous result and square of current element
-template<typename T>
-struct square
-{
-    T operator()(const T& Left, const T& Right) const
-    {   
-        return (Left + Right*Right);
-    }
-};
+////functor for getting sum of previous result and square of current element
+//template<typename T>
+//struct square
+//{
+//    T operator()(const T& Left, const T& Right) const
+//    {
+//        return (Left + Right*Right);
+//    }
+//};
 
 
 //fwhm rms error
-std::vector<double> Data::get_fwhmerr() const
+std::vector<double> Data::create_fwhmerr() const
 {
-    double sum_of_elems;
-    double rms;
-    //sum of the squared elements
-    sum_of_elems = std::accumulate(fwhm.begin(), fwhm.end(), 0, square<int>() );
-
-    //sqrt(sum/n)
-    rms = std::sqrt(sum_of_elems / fwhm.size());
-
-    //now put values into vector
-    std::vector<double> fwhmerr(fwhm.size(), 0.1 * rms);
-
-    //std::cout << "myvector contains:";
-    //for (unsigned i=0; i<fwhmerr.size() ; i++)
-    //    std::cout << ' ' << fwhmerr[i];
-    //std::cout << '\n';
+//    double sum_of_elems;
+//    double rms;
+//    //sum of the squared elements
+//    sum_of_elems = std::accumulate(fwhm.begin(), fwhm.end(), 0, square<int>() );
+//    //sqrt(sum/n)
+//    rms = std::sqrt(sum_of_elems / fwhm.size());
+//    //now put values into vector
+//    std::vector<double> fwhmerr(fwhm.size(), 0.1 * rms);
+//    //std::cout << "myvector contains:";
+//    //for (unsigned i=0; i<fwhmerr.size() ; i++)
+//    //    std::cout << ' ' << fwhmerr[i];
+//    //std::cout << '\n';
+    std::vector<double> fwhmerr;
+    fwhmerr.reserve( rverr.size() ); //preallocate memory
+    for(int n = 0; n < rverr.size(); n++)
+    {
+        fwhmerr.insert( fwhmerr.end(), 2.35 *rverr[n] );
+    }
     return fwhmerr;
 }
 
 
 //BIS rms error
-std::vector<double> Data::get_biserr() const
+std::vector<double> Data::create_biserr() const
 {
-    double sum_of_elems;
-    double rms;
-    //sum of the squared elements
-    sum_of_elems = std::accumulate(bis.begin(), bis.end(), 0, square<int>() );
-    
-    //sqrt(sum/n)
-    rms = std::sqrt(sum_of_elems / bis.size());
-    
-    //now put value into vector
-    std::vector<double> biserr(bis.size(), 0.2 * rms);
+//    double sum_of_elems;
+//    double rms;
+//    //sum of the squared elements
+//    sum_of_elems = std::accumulate(bis.begin(), bis.end(), 0, square<int>() );
+//    //sqrt(sum/n)
+//    rms = std::sqrt(sum_of_elems / bis.size());
+//    //now put value into vector
+//    std::vector<double> biserr(bis.size(), 0.2 * rms);
+    std::vector<double> biserr;
+    biserr.reserve( rverr.size() ); //preallocate memory
+    for(int n = 0; n < rverr.size(); n++)
+    {
+        biserr.insert( biserr.end(), 2.0 *rverr[n] );
+    }
     return biserr;
 }
 
@@ -211,24 +217,14 @@ std::vector<double> Data::get_biserr() const
 //to merge Rvs, fwhm, BIS and Rhk into a single vector
 std::vector<double> Data::get_y() const
 {
-    //merging RVs and fwhm
-    std::vector<double> AB;
-    AB.reserve( rv.size() + fwhm.size() ); // preallocate memory
-    AB.insert( AB.end(), rv.begin(), rv.end() );
-    AB.insert( AB.end(), fwhm.begin(), fwhm.end() );
-    
-    //merging RVs+fwhm with BIS
-    std::vector<double> AC;
-    AC.reserve( AB.size() + bis.size() ); // preallocate memory
-    AC.insert( AC.end(), AB.begin(), AB.end() );
-    AC.insert( AC.end(), bis.begin(), bis.end() );
-    
-    //merging RVs+fwhm+BIS wuth Rhk
-    std::vector<double> AD;
-    AD.reserve( AC.size() + rhk.size() ); // preallocate memory
-    AD.insert( AD.end(), AC.begin(), AC.end() );
-    AD.insert( AD.end(), rhk.begin(), rhk.end() );
-    return AD;
+    std::vector<double> y;
+    y.reserve( rv.size() + fwhm.size() + bis.size() + rhk.size()); //preallocate memory
+    y.insert( y.end(), rv.begin(), rv.end() );
+    y.insert( y.end(), fwhm.begin(), fwhm.end() );
+    y.insert( y.end(), bis.begin(), bis.end() );
+    y.insert( y.end(), rhk.begin(), rhk.end() );
+    printf("%i \n", y.size());
+    return y;
 }
 
 
@@ -236,25 +232,16 @@ std::vector<double> Data::get_y() const
 std::vector<double> Data::get_sig() const
 {
     //merging RVs and fwhm
-    std::vector<double> AB;
-    AB.reserve( rv.size() + fwhm.size() ); // preallocate memory
-    AB.insert( AB.end(), rverr.begin(), rverr.end() );
-    std::vector<double> fwhm_err = get_fwhmerr();
-    AB.insert( AB.end(), fwhm_err.begin(), fwhm_err.end() );
-    
-    //merging RVs+fwhm with BIS
-    std::vector<double> AC;
-    AC.reserve( AB.size() + bis.size() ); // preallocate memory
-    AC.insert( AC.end(), AB.begin(), AB.end() );
-    std::vector<double> bis_err = get_biserr();
-    AC.insert( AC.end(), bis_err.begin(), bis_err.end() );
-    
-    //merging RVs+fwhm+BIS wuth Rhk
-    std::vector<double> AD;
-    AD.reserve( AC.size() + rhk.size() ); // preallocate memory
-    AD.insert( AD.end(), AC.begin(), AC.end() );
-    AD.insert( AD.end(), rhkerr.begin(), rhkerr.end() );   
-    return AD;
+    std::vector<double> sig;
+    sig.reserve(rv.size() + fwhm.size() + bis.size() + rhk.size()); //preallocate memory
+    sig.insert( sig.end(), rverr.begin(), rverr.end() );
+    std::vector<double> fwhmerr = create_fwhmerr();
+    sig.insert( sig.end(), fwhmerr.begin(), fwhmerr.end() );
+    std::vector<double> biserr = create_biserr();
+    sig.insert( sig.end(), biserr.begin(), biserr.end() );
+    sig.insert( sig.end(), rhkerr.begin(), rhkerr.end() );
+    printf("%i \n", sig.size());
+    return sig;
 }
 
 
--- src/Data.h
+++ src/Data.h
@@ -7,48 +7,55 @@
 
 class Data
 {
-	private:
-		std::vector<double> t, rv, rverr, fwhm, bis, rhk, rhkerr, y, sig;
+    private:
+        std::vector<double> t, rv, rverr, fwhm, fwhmerr, bis, biserr, 
+                                                        rhk, rhkerr, tt, y, sig;
 
-	public:
-		Data();
-		//void load(const char* filename);
-		void load(const char* filename, const char* units, int skip=2);
-		int index_fibers;
+    public:
+        Data();
+        //void load(const char* filename);
+        void load(const char* filename, const char* units, int skip=2);
+        int index_fibers;
 
-		const char* datafile;
-		const char* dataunits;
-		int dataskip;
+        const char* datafile;
+        const char* dataunits;
+        int dataskip;
 
-		// Getters
-		int N() const {return t.size();}
+        // Getters
+        int N() const {return t.size();}
 
         //time
-		const std::vector<double>& get_t() const { return t; }
-		double get_t_min() const { return *std::min_element(t.begin(), t.end()); }
-		double get_t_max() const { return *std::max_element(t.begin(), t.end()); }
-		double get_t_middle() const { return get_t_min() + 0.5*(get_t_max() - get_t_min()); }
-		double get_timespan() const { return get_t_max() - get_t_min(); }
+        const std::vector<double>& get_t() const { return t; }
+        double get_t_min() const { return *std::min_element(t.begin(), t.end()); }
+        double get_t_max() const { return *std::max_element(t.begin(), t.end()); }
+        double get_t_middle() const { return get_t_min() + 0.5*(get_t_max() - get_t_min()); }
+        double get_timespan() const { return get_t_max() - get_t_min(); }
 
         //RVs
-		const std::vector<double>& get_rv() const { return rv; }
-		double get_rv_min() const { return *std::min_element(rv.begin(), rv.end()); }
-		double get_rv_max() const { return *std::max_element(rv.begin(), rv.end()); }
-		double get_rv_span() const { return get_rv_max() - get_rv_min(); }
-		double get_rv_var() const;
-		double get_rv_std() const { return std::sqrt(get_rv_var()); }
+        const std::vector<double>& get_rv() const { return rv; }
+        double get_rv_min() const { return *std::min_element(rv.begin(), rv.end()); }
+        double get_rv_max() const { return *std::max_element(rv.begin(), rv.end()); }
+        double get_rv_span() const { return get_rv_max() - get_rv_min(); }
+        double get_rv_var() const;
+        double get_rv_std() const { return std::sqrt(get_rv_var()); }
 
         //RVs error
-		const std::vector<double>& get_rverr() const { return rverr; }
-		
-		//slope
-		double topslope() const {return std::abs(get_rv_max() - get_rv_min()) / (t.back() - t.front());}
-		
-		//The fwhm, BIS, Rhk and respective errors
+        const std::vector<double>& get_rverr() const { return rverr; }
+
+        //slope
+        double topslope() const {return std::abs(get_rv_max() - get_rv_min()) / (t.back() - t.front());}
+
+        //The fwhm, BIS, Rhk and respective errors
         const std::vector<double>& get_fwhm() const { return fwhm; }
-        std::vector<double> get_fwhmerr() const;
+        std::vector<double> create_fwhmerr() const;
+        const std::vector<double>& get_fwhmerr() const { return fwhmerr; } //getter
+        void set_fwhmerr(std::vector<double> create_fwhmerr()) { fwhmerr = create_fwhmerr(); } //setter
+
         const std::vector<double>& get_bis() const { return bis; }
-        std::vector<double> get_biserr() const;
+        std::vector<double> create_biserr() const;
+        const std::vector<double>& get_biserr() const {return biserr; } //getter
+        void set_biserr(std::vector<double> create_biserr()) { biserr = create_biserr(); } //setter
+        
         const std::vector<double>& get_rhk() const { return rhk; }
         const std::vector<double>& get_rhkerr() const { return rhkerr; }
 
@@ -56,11 +63,11 @@ class Data
         std::vector<double> get_y() const;
         std::vector<double> get_sig() const;
         
-	// Singleton
-	private:
-		static Data instance;
-	public:
-		static Data& get_instance() { return instance; }
+    // Singleton
+    private:
+        static Data instance;
+    public:
+        static Data& get_instance() { return instance; }
 };
 
 #endif
