9c78f762bec295d6510c74e2d688a2ba4b848049
João Faria <joao.faria@astro.up.pt>
2018-10-27 18:02:53 +0100

    add some tests to showresults, clean up a bit

--- tests/test_kima.py
+++ tests/test_kima.py
@@ -1,4 +1,5 @@
 import pytest
+import os, sys
 import numpy as np
 import numpy.testing as npt
 
@@ -44,10 +45,10 @@ def test_keplerian():
     npt.assert_allclose(keplerian(times2, 1., 1., 0., 0., 0., 0.), 1.)
 
 
-## pykima.display
+## pykima.utils
 
 def test_percentiles():
-    from pykima.display import percentile68_ranges, percentile68_ranges_latex
+    from pykima.utils import percentile68_ranges, percentile68_ranges_latex
 
     a = np.random.uniform(size=int(1e6))
     b = np.random.randn(int(1e6))
@@ -71,7 +72,7 @@ def test_percentiles():
 
 
 def test_planet_mass():
-    from pykima.display import get_planet_mass
+    from pykima.utils import get_planet_mass
     # get_planet_mass(P, K, e, star_mass=1.0, full_output=False, verbose=False)
     npt.assert_allclose(get_planet_mass(0., 0., 0.), (0., 0.))
     npt.assert_allclose(get_planet_mass(np.random.rand(), 0., np.random.rand()), 
@@ -96,5 +97,73 @@ def test_logsumexp():
 
 ## pykima.showresults
 
-# def test_showresults():
-    # pass
+@pytest.fixture(scope='session')
+def simple_results_dir(tmpdir_factory):
+    """ Create dummy dir with sample.txt, sample_info.txt, and levels.txt """
+    directory = tmpdir_factory.getbasetemp()
+    
+    p = directory.join("sample.txt")
+    p.write("# header\n")
+    p.write("0.0")
+    
+    p = directory.join("sample_info.txt")
+    p.write("# header\n")
+    p.write("0 -1.0e+308 0.0 1")
+    
+    p = directory.join("levels.txt")
+    p.write("# header\n")
+    p.write("0 -1.0e+308 0 2 2 2 2")
+
+    return directory
+
+def write_dummy_model_setup(directory):
+    template_setup = ("[kima] \n"
+                      "obs_after_HARPS_fibers: false \n"
+                      "GP: false \n"
+                      "hyperpriors: false \n"
+                      "trend: false \n"
+
+                      "file: filename.txt \n"
+                      "units: kms \n"
+                      "skip: 0 \n"
+                     )
+    directory.join('kima_model_setup.txt').write(template_setup)
+
+
+def test_showresults_fails(tmpdir, simple_results_dir, capfd):
+    from pykima import showresults
+
+    # in an empty directory, no levels.txt, should fail
+    os.chdir(str(tmpdir))
+    with pytest.raises(SystemExit):
+        showresults()
+
+    # now directory only has levels.txt, but it's empty
+    tmpdir.join("levels.txt").write('# header\n')
+    with pytest.raises(IndexError):
+        showresults()
+
+    # go to the dummy results directory, which has the 3 files, 
+    # but no kima_model_setup.txt
+    os.chdir(str(simple_results_dir))
+    if sys.version_info < (3, 0):
+        with pytest.raises(IOError,
+            match="kima_model_setup.txt"):
+            showresults()
+            # out, err = capfd.readouterr()
+            write_dummy_model_setup(simple_results_dir)
+            with pytest.raises(IOError, match='filename.txt'):
+                showresults()
+    else:
+        with pytest.raises(FileNotFoundError,
+            match="kima_model_setup.txt"):
+            showresults()
+            # out, err = capfd.readouterr()
+    
+        write_dummy_model_setup(simple_results_dir)
+        with pytest.raises(OSError, match='filename.txt'):
+            showresults()
+    
+    # with pytest.raises(SystemExit):
+    #     showresults('wrong_option')
+    
