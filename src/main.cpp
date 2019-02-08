cb14eaf2ba474f8a5ca01d9da0f79ce7c4af9ae7
João Faria <joao.faria@astro.up.pt>
2019-02-08 11:39:46 +0000

    add optional argument to make_template.main; no multi_instrument by default

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ const bool obs_after_HARPS_fibers = false;
 const bool GP = false;
 const bool hyperpriors = false;
 const bool trend = false;
-const bool multi_instrument = true;
+const bool multi_instrument = false;
 
 RVmodel::RVmodel()
 :planets(5, 0, true, RVConditionalPrior())
