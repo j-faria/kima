7d56dd7f20a683d8745dd6853a562d5e6bca8941
João Faria <joao.faria@astro.up.pt>
2019-04-05 16:44:22 +0100

    update options and setup file

--- examples/activity_correlations/kima_setup.cpp
+++ examples/activity_correlations/kima_setup.cpp
@@ -9,39 +9,29 @@ using namespace DNest4;
 
 const bool obs_after_HARPS_fibers = false;
 const bool GP = false;
+const bool MA = false;
 const bool hyperpriors = false;
 const bool trend = false;
 const bool multi_instrument = false;
 
-RVmodel::RVmodel():fix(false),npmax(5)
+RVmodel::RVmodel():fix(true),npmax(0)
 {
     auto data = Data::get_instance();
-    Cprior = new Uniform(data.get_y_min(), data.get_y_max()); // m/s
-    Jprior = new ModifiedLogUniform(1.0, 100.); // m/s
+    // Jprior = make_prior<ModifiedLogUniform>(1.0, 10.); // m/s
 
-    Pprior = new LogUniform(0.2, 1000); // days
-    Kprior = new ModifiedLogUniform(1.0, 10); // m/s
+    auto conditional = planets.get_conditional_prior();
+    conditional->Pprior = make_prior<LogUniform>(1, 10*data.get_timespan()); // days
+    conditional->Kprior = make_prior<ModifiedLogUniform>(1.0, 100); // m/s
 
-    eprior = new Uniform(0., 1.);
-    phiprior = new Uniform(0.0, 2*M_PI);
-    wprior = new Uniform(0.0, 2*M_PI);
-
-    save_setup();
-
+    conditional->eprior = make_prior<Kumaraswamy>(0.867, 3.03);
 }
 
 
 int main(int argc, char** argv)
 {
-    /* set the RV data file */
-    char* datafile = "RVchallenge_system2.rdb";
-    // char* datafile = "dummy2.txt";
+    char* datafile = "dummy1.txt";
+    Data::get_instance().load(datafile, "ms", 0, {"fwhm"});
 
-    // the third (optional) argument, 
-    // tells kima not to skip any line in the header of the file
-    Data::get_instance().load(datafile, "kms", 2, {"fwhm"});
-    
-    // set the sampler and run it!
     Sampler<RVmodel> sampler = setup<RVmodel>(argc, argv);
     sampler.run(50);
 
