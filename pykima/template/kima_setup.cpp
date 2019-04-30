e0f8e40e9bdf4d4eee3ec4dd32aaa49ba2119219
João Faria <joao.faria@astro.up.pt>
2019-04-30 18:52:59 +0100

    improve the description in the template and its options

--- pykima/template/kima_setup.cpp
+++ pykima/template/kima_setup.cpp
@@ -8,35 +8,52 @@ using namespace DNest4;
 
 const bool obs_after_HARPS_fibers = false;
 const bool GP = false;
+const bool MA = false;
 const bool hyperpriors = false;
 const bool trend = false;
 const bool multi_instrument = false;
 
 RVmodel::RVmodel():fix(true),npmax(1)
 {
-    auto data = Data::get_instance();
-    double ymin = data.get_y_min();
-    double ymax = data.get_y_max();
+    // auto data = Data::get_data();
+    // double ymin = data.get_y_min();
+    // double tspan = data.get_timespan();
 
-    // set the prior for the systemic velocity
-    Cprior = new Uniform(ymin, ymax);
+    /// for example, set the prior for the systemic velocity
+    // Cprior = make_prior<Uniform>(0, 1);
+    /// other priors: Jprior, slope_prior, fiber_offset_prior, offsets_prior,
+    ///               log_eta1_prior, eta2_prior, eta3_prior, log_eta4_prior
 
-    save_setup();
+
+    /// for example, set the prior for some planet parameters
+    // auto c = planets.get_conditional_prior();
+    // c->Pprior = make_prior<Gaussian>(0, 1);
+    /// other priors: Kprior, eprior, phiprior, wprior
 }
 
 
 int main(int argc, char** argv)
 {
-    /* set the RV data file */
-    char* datafile = "your data file here";
+    /* only one instrument */
+    char* datafile = "your data file here"; // set the RV data file
 
-    // the third (optional) argument, 
-    // tells kima not to skip any line in the header of the file
+    /// load the file. the second argument sets the units of the RVs (can be 
+    /// either "ms" or "kms") and the third (optional) argument, tells kima not 
+    /// to skip any line in the header of the file
     Data::get_instance().load(datafile, "ms", 0);
     
-    // set the sampler and run it!
+
+    /* more than one instrument */
+    // std::vector<char*> datafiles = {"data file 1", "data file 2"};
+
+    /// load the files, same arguments as for load()
+    // Data::get_instance().load_multi(datafiles, "ms", 0);
+
+
+
+    /// set the sampler and run it!
     Sampler<RVmodel> sampler = setup<RVmodel>(argc, argv);
-    sampler.run();
+    sampler.run(50); // the optional argument to run() sets the print thining
 
     return 0;
 }
