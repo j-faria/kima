100307c3f1844ca42f5d1e7200a7b9c2f8efee04
João Faria <joao.faria@astro.up.pt>
2020-01-15 11:36:45 +0000

    no-colors option to kima-run; catch SIGTERM

--- pykima/run.py
+++ pykima/run.py
@@ -6,8 +6,18 @@ import subprocess
 import argparse
 import time
 import contextlib
+import pipes
+import signal
 
 kimabold = "\033[1mkima\033[0m"
+kimanormal = "kima"
+
+# This is a custom signal handler for SIGTERM
+def receiveSIGTERM(signalNumber, frame):
+    print('kima job terminated, exiting gracefully.', flush=True)
+    sys.exit(15)
+
+signal.signal(signal.SIGTERM, receiveSIGTERM)
 
 
 def _parse_args1():
@@ -17,6 +27,8 @@ def _parse_args1():
 
     parser.add_argument('DIR', nargs='?', default=os.getcwd(),
                         help='change to this directory before running')
+
+    parser.add_argument('--version', action='store_true', help='show version')
 
     parser.add_argument('-t', '--threads', type=int, default=4,
                         help='number of threads to use for the job (default 4)')
@@ -47,9 +59,11 @@ def _parse_args1():
 
     parser.add_argument('--no-notify', action='store_true', default=False,
                         help="do not send notification when job finished")
+    parser.add_argument('--no-colors', action='store_true', default=False,
+                        help=argparse.SUPPRESS)
 
     args = parser.parse_args()
-    return args
+    return args, parser
 
 
 
@@ -99,10 +113,43 @@ def notify(summary, body):
             print(' '.join(cmd))
 
 
+def _change_OPTIONS(postfix):
+    from numpy import loadtxt
+    shutil.copy('OPTIONS', 'OPTIONS.bak')
+
+    original_lines = [l.strip() for l in open('OPTIONS').readlines()]
+    lines = [line for line in original_lines if not line.startswith('#')]
+    option_values = loadtxt('OPTIONS', dtype=int)
+
+    sample = 'sample_%s.txt' % postfix
+    levels = 'levels_%s.txt' % postfix
+    sample_info = 'sample_info_%s.txt' % postfix
+
+    with open('OPTIONS', 'w') as f:
+        for line in original_lines:
+            if line.startswith('#'):
+                print(line, file=f)
+            else:
+                break
+
+        for v, l in zip(option_values, lines):
+            print(v, '\t# ' + l.split('#')[1], file=f)
+
+        print(sample, '\t# samples file', file=f)
+        print(sample_info, '\t# sample_info file', file=f)
+        print(levels, '\t# levels file', file=f)
+
+
 def run_local():
     """ Run kima jobs """
-    args = _parse_args1()
+    args, parser = _parse_args1()
     # print(args)
+    if args.version:
+        version_file = os.path.join(os.path.dirname(__file__), '../VERSION')
+        v = open(version_file).read().strip() # same as kima
+        print('kima (%s script)' % parser.prog, v)
+        sys.exit(0)
+
 
     with remember_cwd():
 
@@ -132,7 +179,7 @@ def run_local():
                 if args.vc:
                     print()
                     print(make.decode().strip())
-                print('done!')
+                print('done!', flush=True)
 
         except subprocess.CalledProcessError as e:
             print("{}: {}".format(type(e).__name__, e))
@@ -156,8 +203,10 @@ def run_local():
 
         TO = args.timeout
 
+        kimastr = kimanormal if args.no_colors else kimabold
+
         if not args.quiet:
-            print('starting', kimabold)
+            print('starting', kimastr, flush=True)
 
         start = time.time()
         try:
@@ -177,7 +226,7 @@ def run_local():
             end = time.time()
             took = end - start
             if not args.quiet:
-                print(kimabold, 'job timed out after %.1f seconds' % took,
+                print(kimastr, 'job timed out after %.1f seconds' % took,
                       end=' ')
                 print('(saved %d samples)' % rawgencount('sample.txt'))
             if not args.no_notify:
@@ -188,8 +237,7 @@ def run_local():
             end = time.time()
             took = end - start
             if not args.quiet:
-                print(kimabold,
-                      'job finished, took %.2f seconds' % (end - start))
+                print(kimastr, 'job finished, took %.2f seconds' % (end - start))
             if not args.no_notify:
                 notify('kima job finished', 'took %.2f seconds' % took)
         finally:
