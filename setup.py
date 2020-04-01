de77739f8fe678965c4859a2788c9e6af2ddc7dc
João Faria <joao.faria@astro.up.pt>
2020-04-01 17:42:00 +0100

    formatting

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ from __future__ import print_function
 import os
 from setuptools import setup
 
+
 def fill_make_template():
-  thisdir = os.path.dirname(os.path.realpath(__file__))
-  maketemplate = os.path.join(thisdir, 'pykima', 'make_template.template')
-  maketemplatepy = os.path.join(thisdir, 'pykima', 'make_template.py')
-  pycode = open(maketemplate).read().format(kimadir=thisdir)
-  print(pycode, file=open(maketemplatepy, 'w'))
+    thisdir = os.path.dirname(os.path.realpath(__file__))
+    maketemplate = os.path.join(thisdir, 'pykima', 'make_template.template')
+    maketemplatepy = os.path.join(thisdir, 'pykima', 'make_template.py')
+    pycode = open(maketemplate).read().format(kimadir=thisdir)
+    print(pycode, file=open(maketemplatepy, 'w'))
+
 
 fill_make_template()
 
 
 setup(name='pykima',
-      version=open('VERSION').read().strip(), # same as kima
+      version=open('VERSION').read().strip(),  # same as kima
       description='Analysis of results from kima',
       author='João Faria',
       author_email='joao.faria@astro.up.pt',
@@ -24,22 +26,22 @@ setup(name='pykima',
       url='https://github.com/j-faria/kima/tree/master/pykima',
       packages=['pykima'],
       install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib>=1.5.3',
-        'corner',
-        'loguniform',
-        'kumaraswamy',
+          'numpy',
+          'scipy',
+          'matplotlib>=1.5.3',
+          'corner',
+          'loguniform',
+          'kumaraswamy',
       ],
       entry_points={
-        'console_scripts': [
-            'kima-showresults = pykima.showresults:showresults',
-            'kima-checkpriors = pykima.check_priors:main',
-            'kima-template = pykima.make_template:main',
-            'kima-run = pykima.run:run_local',
-            'kima-gui = pykima.kimaGUI:main',
-            ]
-        },
+          'console_scripts': [
+              'kima-showresults = pykima.showresults:showresults',
+              'kima-checkpriors = pykima.check_priors:main',
+              'kima-template = pykima.make_template:main',
+              'kima-run = pykima.run:run_local',
+              'kima-gui = pykima.kimaGUI:main',
+          ]
+      },
       package_data={'pykima': ['template/*']},
       include_package_data=True,
-     )
+      )
