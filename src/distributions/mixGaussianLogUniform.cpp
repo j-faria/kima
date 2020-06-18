42d155edf437f2959aa94dad491bac25d66a260c
João Faria <joao.faria@astro.up.pt>
2020-06-18 19:37:20 +0100

    fixes to the mixture of Gaussian and LogUniform distributions

--- src/distributions/mixGaussianLogUniform.cpp
+++ src/distributions/mixGaussianLogUniform.cpp
@@ -4,31 +4,50 @@ namespace DNest4
 {
 
 /**
- * Construct an mixGaussianLogUniform distribution
+ * Construct a mixGaussianLogUniform distribution
 */
 mixGaussianLogUniform::mixGaussianLogUniform(double mean, double sigma, double lower, double upper)
+: mean(mean), sigma(sigma), lower(lower), upper(upper)
 {
+    // the two components
     G = Gaussian(mean, sigma);
     LU = LogUniform(lower, upper);
 }
 
 
-/* mixGaussianLogUniform distribution function */
+/* mixGaussianLogUniform cumulative distribution function */
 double mixGaussianLogUniform::cdf(double x) const
 {
     return 0.5 * G.cdf(x) + 0.5 * LU.cdf(x);
 }
 
-/* Quantile estimate, midpoint interpolation */
+/* mixGaussianLogUniform quantile function */
 double mixGaussianLogUniform::cdf_inverse(double p) const
 {
     // not implemented
-    throw std::runtime_error("cdf_inverse not implemented for `mixGaussianLogUniform`");
+    // throw std::runtime_error("cdf_inverse not implemented for `mixGaussianLogUniform`");
+
+    // estimate cdf_inverse with bisection
+    double tol = 2e-12, a = lower, b = upper;
+    double c = a;
+
+    while ((b - a) >= tol)
+    {
+        c = (a + b) / 2;
+        if (cdf(c) - p == 0.0)
+            break;
+        else if ((cdf(c) - p) * (cdf(a) - p) < 0)
+            b = c;
+        else
+            a = c;
+    }
+
+    return c;
 }
 
 double mixGaussianLogUniform::log_pdf(double x) const
 {
-    return 0.5 * G.log_pdf(x) + 0.5 * LU.log_pdf(x);
+    return log(0.5 * exp(G.log_pdf(x)) + 0.5 * exp(LU.log_pdf(x)));
 }
 
 double mixGaussianLogUniform::perturb(double& x, RNG& rng) const
--- src/distributions/mixGaussianLogUniform.h
+++ src/distributions/mixGaussianLogUniform.h
@@ -3,13 +3,7 @@
 // DNest4/code
 #include "DNest4.h"
 #include <limits>
-#include <stdlib.h>
-#include <stdint.h>
-#include <cmath>
 #include <iostream>
-#include <fstream>
-#include <vector> 
-#include <algorithm>
 
 using namespace std;
 
@@ -19,16 +13,18 @@ namespace DNest4
 class mixGaussianLogUniform:public ContinuousDistribution
 {
     private:
+        double mean, sigma, lower, upper;
         DNest4::Gaussian G;
         DNest4::LogUniform LU;
 
     public:
-        mixGaussianLogUniform(double mean=0.0, double sigma=1.0, double lower=1.0, double upper=2.0);
+        mixGaussianLogUniform(double mean = 0.0, double sigma = 1.0,
+                              double lower = 1.0, double upper = 2.0);
 
         double cdf(double x) const;
         double cdf_inverse(double p) const;
         double log_pdf(double x) const;
-        double perturb(double& x, RNG& rng) const override;
+        double perturb(double& x, RNG& rng) const;
 };
 
 
