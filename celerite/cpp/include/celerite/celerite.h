68de2345b956bedd3491480599f5012c89934e8d
João Faria <joao.faria@astro.up.pt>
2022-11-02 17:34:16 +0000

    remove stuff

--- celerite/cpp/include/celerite/celerite.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CELERITE_CELERITE_H_
-#define _CELERITE_CELERITE_H_
-
-#include "celerite/version.h"
-#include "celerite/exceptions.h"
-#include "celerite/solver/direct.h"
-#include "celerite/solver/cholesky.h"
-
-#endif
