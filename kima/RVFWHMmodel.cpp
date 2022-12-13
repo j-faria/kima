5ff23d7490741cb1ac9baecf733176e527778d67
João Faria <joao.faria@astro.up.pt>
2022-12-13 19:09:03 +0000

    fixing a number of issues with RVFWHM model

--- kima/RVFWHMmodel.cpp
+++ kima/RVFWHMmodel.cpp
@@ -9,17 +9,17 @@ const double halflog2pi = 0.5*log(2.*M_PI);
 /// set default priors if the user didn't change them
 void RVFWHMmodel::setPriors()  // BUG: should be done by only one thread!
 {
+    auto minFWHM = *min_element(data.actind[0].begin(), data.actind[0].end());
+    auto maxFWHM = *max_element(data.actind[0].begin(), data.actind[0].end());
+    auto spanFWHM = maxFWHM - minFWHM;
+
     // systemic velocity
     if (!Cprior)
         Cprior = make_prior<Uniform>(data.get_RV_min(), data.get_RV_max());
     
     // "systemic FWHM"
     if (!C2prior)
-    {
-        auto minFWHM = *min_element(data.actind[0].begin(), data.actind[0].end());
-        auto maxFWHM = *max_element(data.actind[0].begin(), data.actind[0].end());
         C2prior = make_prior<Uniform>(minFWHM, maxFWHM);
-    }
 
     // jitter for the RVs
     if (!Jprior)
@@ -28,13 +28,7 @@ void RVFWHMmodel::setPriors()  // BUG: should be done by only one thread!
 
     // jitter for the FWHM
     if (!J2prior)
-    {
-        auto minFWHM = *min_element(data.actind[0].begin(), data.actind[0].end());
-        auto maxFWHM = *max_element(data.actind[0].begin(), data.actind[0].end());
-        auto spanFWHM = maxFWHM - minFWHM;
-        J2prior = make_prior<ModifiedLogUniform>(
-            min(1.0, 0.1 * spanFWHM), spanFWHM);
-    }
+        J2prior = make_prior<ModifiedLogUniform>(min(1.0, 0.1 * spanFWHM), spanFWHM);
 
     if (trend){
         if (degree == 0)
@@ -55,9 +49,6 @@ void RVFWHMmodel::setPriors()  // BUG: should be done by only one thread!
         if (!offsets_prior)
             offsets_prior = make_prior<Uniform>( -data.get_RV_span(), data.get_RV_span() );
         if (!offsets_fwhm_prior) {
-            auto minFWHM = *min_element(data.actind[0].begin(), data.actind[0].end());
-            auto maxFWHM = *max_element(data.actind[0].begin(), data.actind[0].end());
-            auto spanFWHM = maxFWHM - minFWHM;
             offsets_fwhm_prior = make_prior<Uniform>( -spanFWHM, spanFWHM );
         }
 
@@ -82,14 +73,27 @@ void RVFWHMmodel::setPriors()  // BUG: should be done by only one thread!
     /* GP parameters */
     if (!eta1_prior)
         eta1_prior = make_prior<LogUniform>(0.1, 100);
+    if (!eta1_fwhm_prior)
+        eta1_fwhm_prior = make_prior<LogUniform>(0.1, spanFWHM);
+
     if (!eta2_prior)
         eta2_prior = make_prior<LogUniform>(1, 100);
+    if (!eta2_fwhm_prior && !share_eta2)
+        eta2_fwhm_prior = make_prior<LogUniform>(1, 100);
+
     if (!eta3_prior)
         eta3_prior = make_prior<Uniform>(10, 40);
-    if (!eta4_prior && kernel != celerite)
+    if (!eta3_fwhm_prior && !share_eta3)
+        eta3_fwhm_prior = make_prior<Uniform>(10, 40);
+
+    if (!eta4_prior)
         eta4_prior = make_prior<Uniform>(0.2, 5);
+    if (!eta4_fwhm_prior && !share_eta4)
+        eta4_fwhm_prior = make_prior<Uniform>(0.2, 5);
+
     if (!alpha_prior && kernel == perrq)
         alpha_prior = make_prior<LogUniform>(0.5, 10);
+
     if (!eta5_prior && kernel == qpc)
         eta5_prior = make_prior<Uniform>(0, 10);
 
@@ -98,100 +102,104 @@ void RVFWHMmodel::setPriors()  // BUG: should be done by only one thread!
 
 void RVFWHMmodel::from_prior(RNG& rng)
 {
+    if (data.actind.size() < 2) {
+        std::cout << std::endl;
+        throw std::runtime_error("no activity indicators were loaded");
+    }
+
     // preliminaries
-    // setPriors();
-    // save_setup();
+    setPriors();
+    save_setup();
 
 
-    // planets.from_prior(rng);
-    // planets.consolidate_diff();
+    planets.from_prior(rng);
+    planets.consolidate_diff();
 
-    // bkg = Cprior->generate(rng);
-    // bkg_fwhm = C2prior->generate(rng);
+    bkg = Cprior->generate(rng);
+    bkg_fwhm = C2prior->generate(rng);
 
-    // if(data.datamulti)
-    // {
-    //     // draw instrument offsets for the RVs
-    //     for (size_t i = 0; i < offsets.size() / 2; i++)
-    //         offsets[i] = individual_offset_prior[i]->generate(rng);
-    //     // draw instrument offsets for the FWHM
-    //     for (size_t i = offsets.size() / 2; i < offsets.size(); i++)
-    //         offsets[i] = individual_offset_fwhm_prior[i]->generate(rng);
+    if(data.datamulti)
+    {
+        // draw instrument offsets for the RVs
+        for (size_t i = 0; i < offsets.size() / 2; i++)
+            offsets[i] = offsets_prior->generate(rng);
+        // draw instrument offsets for the FWHM
+        for (size_t i = offsets.size() / 2; i < offsets.size(); i++)
+            offsets[i] = offsets_fwhm_prior->generate(rng);
 
-    //     for (size_t i = 0; i < jitters.size() / 2; i++)
-    //         jitters[i] = Jprior->generate(rng);
-    //     for (size_t i = jitters.size() / 2; i < jitters.size(); i++)
-    //         jitters[i] = J2prior->generate(rng);
-    // }
-    // else
-    // {
-    //     jitter = Jprior->generate(rng);
-    //     jitter_fwhm = J2prior->generate(rng);
-    // }
+        for (size_t i = 0; i < jitters.size() / 2; i++)
+            jitters[i] = Jprior->generate(rng);
+        for (size_t i = jitters.size() / 2; i < jitters.size(); i++)
+            jitters[i] = J2prior->generate(rng);
+    }
+    else
+    {
+        jitter = Jprior->generate(rng);
+        jitter_fwhm = J2prior->generate(rng);
+    }
+
+    if(trend)
+    {
+        if (degree >= 1) slope = slope_prior->generate(rng);
+        if (degree >= 2) quadr = quadr_prior->generate(rng);
+        if (degree == 3) cubic = cubic_prior->generate(rng);
+    }
 
 
-    // if(trend)
-    // {
-    //     if (degree >= 1) slope = slope_prior->generate(rng);
-    //     if (degree >= 2) quadr = quadr_prior->generate(rng);
-    //     if (degree == 3) cubic = cubic_prior->generate(rng);
-    // }
+    if (known_object) { // KO mode!
+        KO_P.resize(n_known_object);
+        KO_K.resize(n_known_object);
+        KO_e.resize(n_known_object);
+        KO_phi.resize(n_known_object);
+        KO_w.resize(n_known_object);
 
+        for (int i=0; i<n_known_object; i++){
+            KO_P[i] = KO_Pprior[i]->generate(rng);
+            KO_K[i] = KO_Kprior[i]->generate(rng);
+            KO_e[i] = KO_eprior[i]->generate(rng);
+            KO_phi[i] = KO_phiprior[i]->generate(rng);
+            KO_w[i] = KO_wprior[i]->generate(rng);
+        }
+    }
 
-    // if (known_object) { // KO mode!
-    //     KO_P.resize(n_known_object);
-    //     KO_K.resize(n_known_object);
-    //     KO_e.resize(n_known_object);
-    //     KO_phi.resize(n_known_object);
-    //     KO_w.resize(n_known_object);
+    // GP
+    eta1 = eta1_prior->generate(rng);  // m/s
+    eta1_fw = eta1_fwhm_prior->generate(rng);  // m/s
 
-    //     for (int i=0; i<n_known_object; i++){
-    //         KO_P[i] = KO_Pprior[i]->generate(rng);
-    //         KO_K[i] = KO_Kprior[i]->generate(rng);
-    //         KO_e[i] = KO_eprior[i]->generate(rng);
-    //         KO_phi[i] = KO_phiprior[i]->generate(rng);
-    //         KO_w[i] = KO_wprior[i]->generate(rng);
-    //     }
-    // }
+    if (kernel == sqexp)
+    {
+        eta2 = eta2_prior->generate(rng); // days
+        if (!share_eta2)
+            eta2_fw = eta2_fwhm_prior->generate(rng); // days
+    }
+    else
+    {
+        eta3 = eta3_prior->generate(rng); // days
+        if (!share_eta3)
+            eta3_fw = eta3_fwhm_prior->generate(rng); // days
 
-    // // GP
-    // eta1 = eta1_prior->generate(rng);  // m/s
-    // eta1_fw = eta1_fwhm_prior->generate(rng);  // m/s
+        eta2 = eta2_prior->generate(rng); // days
+        if (!share_eta2)
+            eta2_fw = eta2_fwhm_prior->generate(rng); // days
 
-    // if (kernel == sqexp)
-    // {
-    //     eta2 = eta2_prior->generate(rng); // days
-    //     if (!share_eta2)
-    //         eta2_fw = eta2_fwhm_prior->generate(rng); // days
-    // }
-    // else
-    // {
-    //     eta3 = eta3_prior->generate(rng); // days
-    //     if (!share_eta3)
-    //         eta3_fw = eta3_fwhm_prior->generate(rng); // days
-
-    //     eta2 = eta2_prior->generate(rng); // days
-    //     if (!share_eta2)
-    //         eta2_fw = eta2_fwhm_prior->generate(rng); // days
-
-    //     eta4 = exp(eta4_prior->generate(rng));
-    //     if (!share_eta4)
-    //         eta4_fw = eta4_fwhm_prior->generate(rng);
+        eta4 = exp(eta4_prior->generate(rng));
+        if (!share_eta4)
+            eta4_fw = eta4_fwhm_prior->generate(rng);
         
-    //     if (kernel == perrq)
-    //         alpha = alpha_prior->generate(rng);
-    //         //! missing FWHM
+        if (kernel == perrq)
+            alpha = alpha_prior->generate(rng);
+            //! missing FWHM
 
-    //     if (kernel == qpc)
-    //         eta5 = eta5_prior->generate(rng);
-    //         //! missing FWHM
-    // }
+        if (kernel == qpc)
+            eta5 = eta5_prior->generate(rng);
+            //! missing FWHM
+    }
 
-    // calculate_mu();
-    // calculate_mu_fwhm();
+    calculate_mu();
+    calculate_mu_fwhm();
 
-    // calculate_C();
-    // calculate_C_fwhm();
+    calculate_C();
+    calculate_C_fwhm();
 }
 
 /// @brief Calculate the full RV model
@@ -1048,12 +1056,14 @@ double RVFWHMmodel::perturb(RNG& rng)
 
         // propose new vsys
         Cprior->perturb(bkg, rng);
+        C2prior->perturb(bkg_fwhm, rng);
 
         // propose new instrument offsets
         if (data.datamulti){
-            for(unsigned j=0; j<offsets.size(); j++){
-                individual_offset_prior[j]->perturb(offsets[j], rng);
-            }
+            for (size_t j = 0; j < offsets.size() / 2; j++)
+                offsets_prior->perturb(offsets[j], rng);
+            for (size_t j = offsets.size() / 2; j < offsets.size(); j++)
+                offsets_fwhm_prior->perturb(offsets[j], rng);
         }
 
         // propose new slope
@@ -1078,6 +1088,9 @@ double RVFWHMmodel::perturb(RNG& rng)
                 }
             }
         }
+
+        calculate_mu_fwhm();
+
     }
 
 
@@ -1100,7 +1113,6 @@ double RVFWHMmodel::log_likelihood() const
     const auto& obsi = data.get_obsi();
 
     double logL = 0.;
-    return logL;
 
     if (enforce_stability){
         int stable = is_stable();
@@ -1113,29 +1125,42 @@ double RVFWHMmodel::log_likelihood() const
     auto begin = std::chrono::high_resolution_clock::now();  // start timing
     #endif
 
-    /** The following code calculates the log likelihood of a GP model */
     // residual vector (observed y minus model y)
     VectorXd residual(y.size());
     for (size_t i = 0; i < y.size(); i++)
         residual(i) = y[i] - mu[i];
 
-    // perform the cholesky decomposition of C
-    Eigen::LLT<Eigen::MatrixXd> cholesky = C.llt();
+    // perform the cholesky decomposition of the covariance matrix
+    Eigen::LLT<Eigen::MatrixXd> cholesky;
+    cholesky = C.llt();
     // get the lower triangular matrix L
     MatrixXd L = cholesky.matrixL();
-
     double logDeterminant = 0.;
-    for (size_t i = 0; i < y.size(); i++)
+    for (size_t i = 0; i < N; i++)
         logDeterminant += 2. * log(L(i, i));
 
     VectorXd solution = cholesky.solve(residual);
-
     // y*solution
     double exponent = 0.;
-    for (size_t i = 0; i < y.size(); i++)
+    for (size_t i = 0; i < N; i++)
         exponent += residual(i) * solution(i);
 
-    logL = -0.5*y.size()*log(2*M_PI) - 0.5*logDeterminant - 0.5*exponent;
+    logL += -0.5 * N * log(2 * M_PI) - 0.5 * logDeterminant - 0.5 * exponent;
+
+    // 2nd output
+    for (size_t i = 0; i < N; i++)
+        residual(i) = data.actind[0][i] - mu_fwhm[i];
+
+    cholesky = C_fwhm.llt();
+    L = cholesky.matrixL();
+    logDeterminant = 0.;
+    for (size_t i = 0; i < N; i++)
+        logDeterminant += 2. * log(L(i, i));
+    solution = cholesky.solve(residual);
+    exponent = 0.;
+    for (size_t i = 0; i < N; i++)
+        exponent += residual(i) * solution(i);
+    logL += -0.5 * N * log(2 * M_PI) - 0.5 * logDeterminant - 0.5 * exponent;
 
 
     #if TIMING
@@ -1162,8 +1187,10 @@ void RVFWHMmodel::print(std::ostream& out) const
         for (int j = 0; j < jitters.size(); j++)
             out << jitters[j] << '\t';
     }
-    else
+    else {
         out << jitter << '\t';
+        out << jitter_fwhm << '\t';
+    }
 
     if(trend)
     {
@@ -1182,18 +1209,22 @@ void RVFWHMmodel::print(std::ostream& out) const
     }
 
     // write GP parameters
-    if (kernel == sqexp)
-        out << eta1 << '\t' << eta2 << '\t';
-    else if (kernel == periodic)
-        out << eta1 << '\t' << eta3 << '\t' << eta4 << '\t';
-    else if (kernel != celerite)
-        out << eta1 << '\t' << eta2 << '\t' << eta3 << '\t' << eta4 << '\t';
-    else
-        out << eta1 << '\t' << eta2 << '\t' << eta3 << '\t';
-    if (kernel == perrq)
-        out << alpha << '\t';
-    if (kernel == qpc)
+    out << eta1 << '\t' << eta1_fw << '\t';
+    
+    out << eta2 << '\t';
+    if (!share_eta2) out << eta2_fw << '\t';
+    
+    out << eta3 << '\t';
+    if (!share_eta3) out << eta3_fw << '\t';
+    
+    out << eta4 << '\t';
+    if (!share_eta4) out << eta4_fw << '\t';
+
+    if (kernel == qpc){
         out << eta5 << '\t';
+        if (!share_eta5) out << eta5_fw << '\t';
+    }
+
 
     // write KO parameters
     if(known_object){ // KO mode!
@@ -1244,20 +1275,22 @@ string RVFWHMmodel::description() const
     }
 
     // GP parameters
-    if (kernel == sqexp)
-        desc += "eta1" + sep + "eta2" + sep;
-    else if (kernel == periodic)
-        desc += "eta1" + sep + "eta3" + sep + "eta4" + sep;
-    else if (kernel != celerite)
-        desc += "eta1" + sep + "eta2" + sep + "eta3" + sep + "eta4" + sep;
-    else
-        desc += "eta1" + sep + "eta2" + sep + "eta3" + sep;
+    desc += "eta1" + sep + "eta1_fw" + sep;
 
-    if (kernel == perrq)
-        desc += "alpha" + sep;
-    
-    if (kernel == qpc)
+    desc += "eta2" + sep;
+    if (!share_eta2) desc += "eta2_fw" + sep;
+
+    desc += "eta3" + sep;
+    if (!share_eta3) desc += "eta3_fw" + sep;
+
+    desc += "eta4" + sep;
+    if (!share_eta4) desc += "eta4_fw" + sep;
+
+    if (kernel == qpc){
         desc += "eta5" + sep;
+        if (!share_eta5) desc += "eta5_fw" + sep;
+    }
+
 
     if(known_object) { // KO mode!
         for(int i=0; i<n_known_object; i++) 
@@ -1289,6 +1322,7 @@ string RVFWHMmodel::description() const
 
     desc += "staleness" + sep;
 
+    desc += "C2" + sep;
     desc += "vsys";
 
     return desc;
@@ -1310,6 +1344,13 @@ void RVFWHMmodel::save_setup() {
     fout << "npmax: " << npmax << endl << endl;
 
     fout << "GP: " << true << endl;
+    fout << "GP_kernel: " << _kernels[kernel] << endl;
+    fout << "share_eta2: " << share_eta2 << endl;
+    fout << "share_eta3: " << share_eta3 << endl;
+    fout << "share_eta4: " << share_eta4 << endl;
+    fout << "share_eta5: " << share_eta5 << endl;
+    fout << "share_eta6: " << share_eta6 << endl;
+
     fout << "hyperpriors: " << false << endl;
     fout << "trend: " << trend << endl;
     fout << "degree: " << degree << endl;
